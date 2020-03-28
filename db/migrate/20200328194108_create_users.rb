31e409a72dfb3ea0ed46d70ba45ba8e7c41715dc
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-28 19:41:27 +0000

    Automatically backed up by Learn

--- db/migrate/20200328194108_create_users.rb
+++ db/migrate/20200328194108_create_users.rb
@@ -1,4 +1,9 @@
-class CreateUsers.rb < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+    end
   end
 end
